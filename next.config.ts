a705f3563444d1eec76161ef6773206b727c0803
MajorTom <invaderzgame@gmail.com>
2024-12-16 20:04:18 +0700

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
